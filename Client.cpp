d1cc3c246991249309984b9cb3c7fe36649a02c7
hitenderprakash <hitenderprakash@gmail.com>
2018-09-17 19:48:27 -0400

    Added null check for interface pointer, in case dll is not present or any other error

--- Client.cpp
+++ Client.cpp
@@ -6,8 +6,16 @@ using namespace std;
 
 int main(){
 	IUnknown* pIUnknown = CallCreateCComponentInstance("CComponentClass.dll");
+	if(pIUnknown == NULL){
+		std::cout<<"\nError in loading the COM Server dll";
+		return 0;
+	}
 	IComponent* pIComponent=NULL;
 	pIUnknown->QueryInterface(IID_IComponent,(void**)&pIComponent);
+	if(pIComponent == NULL){
+		std::cout<<"\nError in getting the requested Interface";
+		return 0;
+	}
 	std::cout<<"\nSum of 9 and 4 is: "<< pIComponent->add(9,4);
 	std::cout<<"\ndifference of 9 and 4 is: "<< pIComponent->subtract(9,4);
 	pIComponent->Release();
